2d56962b0c5ec5672bc041d0dac478902e7e9085
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-02 23:54:41 +0100

    created sidebar component

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="react-scripts" />
 declare module "*.png";
-declare module "*.svg";
+declare module "*.svg" {
+  import { ReactElement, SVGProps } from "react";
+  const content: (props: SVGProps<SVGElement>) => ReactElement;
+  export default content;
+}
+// declare module "*.svg" {
+//   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+//   export default content;
+// }
+// querySelectorAll<K extends keyof HTMLElementTagNameMap>(selectors: K): NodeListOf<HTMLElementTagNameMap[K]>;
+// querySelectorAll<E extends Element = Element>(selectors: string): NodeListOf<E>;
