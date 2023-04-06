f2c92c559b540bd4615ee101f73951a24963f20e
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-06 07:44:04 +0100

    feat: added show/hide button functionality

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,34 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPage.scss";
 import { Link } from "react-router-dom";
 import Logo from "../../Assets/logo.svg";
 import SignInImage from "../../Assets/signInImage.png";
 
 const LoginPage = () => {
-  const password = document.querySelector("#pwd");
-  const toggleBtn = document.querySelector("#togglePwd");
-  toggleBtn?.addEventListener("click", function () {
-    // console.log(password?.getAttribute("type"));
-    //toggle type attribute
-    const type =
-      password?.getAttribute("type") === "password" ? "text" : "password";
-    password?.setAttribute("type", type);
-  });
-
-  //prevent form submit
-  const form = document.querySelector("form");
-  form?.addEventListener("submit", function (e) {
-    e.preventDefault();
-  });
-
-  // const showPwd = () => {
-  //   const showPwdBtn = document.getElementById("pwd");
-  //   if (showPwdBtn.type === "password") {
-  //     showPwdBtn.type === "text";
-  //   } else {
-  //     showPwdBtn.type === "password";
-  //   }
-  // };
+  const [text, setText] = useState(false);
+  const toggle = () => {
+    setText((prev) => !prev);
+  };
 
   return (
     <div className="loginPage">
@@ -40,15 +20,10 @@ const LoginPage = () => {
         <h1>Welcome!</h1>
         <h3>Enter details to login.</h3>
         <form>
-          <input type="email" name="email" placeholder="Email" />
+          <input type="email" placeholder="Email" />
           <label>
-            <input
-              type="password"
-              id="pwd"
-              name="password"
-              placeholder="Password"
-            />
-            <button id="togglePwd">SHOW</button>
+            <input type={text ? "text" : "password"} placeholder="Password" />
+            <span onClick={toggle}>{text ? "HIDE" : "SHOW"}</span>
           </label>
           <span> FORGOT PASSWORD?</span>
           <br />
