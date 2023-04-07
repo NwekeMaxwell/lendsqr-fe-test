fd116edc2fc1de62549d4f1844fa5ce31d394913
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-07 08:19:41 +0100

    feat: dynamic functionality

--- src/Components/UsersListBox/UsersListBox.tsx
+++ src/Components/UsersListBox/UsersListBox.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./UsersListBox.scss";
 import iFilter from "../../Assets/filter-results-button.svg";
 import List from "../List/List";
 import { Link } from "react-router-dom";
-import useFetchData from "../../useFetchData";
-import { iContext } from "../../useFetchData";
+import useFetchData, { iContext } from "../../useFetchData";
+import { UserContext } from "../../AppContext";
 
 const UsersListBox = () => {
   const { userData } = useFetchData();
+  const { user, setUser } = useContext(UserContext);
 
+  const toggle = () =>
+    user.clicked === false
+      ? setUser({ clicked: true })
+      : setUser({ clicked: false });
+  // console.log(user.clicked);
   // console.log(userData);
 
   const start = 0;
@@ -20,37 +26,37 @@ const UsersListBox = () => {
         <span>
           ORGANIZATION
           <Link to="/users">
-            <img src={iFilter} alt="" className="iFilter" />
+            <img onClick={toggle} src={iFilter} alt="" className="iFilter" />
           </Link>
         </span>
         <span>
           USERNAME
           <Link to="/users">
-            <img src={iFilter} alt="" className="iFilter" />
+            <img onClick={toggle} src={iFilter} alt="" className="iFilter" />
           </Link>
         </span>
         <span>
           EMAIL
           <Link to="/users">
-            <img src={iFilter} alt="" className="iFilter" />
+            <img onClick={toggle} src={iFilter} alt="" className="iFilter" />
           </Link>
         </span>
         <span>
           PHONE NUMBER
           <Link to="/users">
-            <img src={iFilter} alt="" className="iFilter" />
+            <img onClick={toggle} src={iFilter} alt="" className="iFilter" />
           </Link>
         </span>
         <span>
           DATE JOINED
           <Link to="/users">
-            <img src={iFilter} alt="" className="iFilter" />
+            <img onClick={toggle} src={iFilter} alt="" className="iFilter" />
           </Link>
         </span>
         <span>
           STATUS
           <Link to="/users">
-            <img src={iFilter} alt="" className="iFilter" />
+            <img onClick={toggle} src={iFilter} alt="" className="iFilter" />
           </Link>
         </span>
       </div>
