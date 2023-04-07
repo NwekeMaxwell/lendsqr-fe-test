b028af374142d1e44aabbb9a872565241af9b86f
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-07 06:08:54 +0100

    fix: responsive layout

--- src/Components/PageHeader/PageHeader.tsx
+++ src/Components/PageHeader/PageHeader.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PageHeader.scss";
 import Logo from "../../Assets/logo.svg";
 import SearchIcon from "../../Assets/searchIcon.svg";
 import BellIcon from "../../Assets/bellIcon.svg";
 import Avatar from "../../Assets/avatar.jpg";
 import Arrow from "../../Assets/arrowDownIcon.svg";
+import Menu from "../../Assets/menu.svg";
+import Cross from "../../Assets/cross.svg";
 
 const PageHeader = () => {
+  const [click, setClicked] = useState(false);
+  const toggle = () => {
+    setClicked((prev) => !prev);
+  };
+
   return (
     <div className="header">
       <div className="left">
         <img src={Logo} alt="logo" className="logo" />
+        <img
+          onClick={toggle}
+          src={click ? Cross : Menu}
+          alt="logo"
+          className="menuIcon"
+        />
         <div className="searchField">
           <input type="search" placeholder="search for anything" />
           <div className="searchIcon">
